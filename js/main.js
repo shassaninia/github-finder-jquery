4ad94759117c61d8d1fd2ec754e0628fcfdb76ef
shassaninia <xai.soft80@gmail.com>
2017-01-30 11:26:39 -0500

    test

--- js/main.js
+++ js/main.js
@@ -40,7 +40,6 @@ $(document).ready(function(){
           `);
         });
       });
-      
       $('#profile').html(`
         <div class="panel panel-default">
           <div class="panel-heading">
